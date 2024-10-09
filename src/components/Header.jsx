8f79ca1af63c0e6a1a559fd38c3bda23d429f0d9
Akash Das <akashd2664@gmail.com>
2024-10-09 10:44:22 +0530

    typo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(props) {
   return (
     <header>
-      <h1>Dairy</h1>
+      <h1>Diary</h1>
       <div className="bin" onClick={props.displayBin}>🗑️</div>
     </header>
   );
